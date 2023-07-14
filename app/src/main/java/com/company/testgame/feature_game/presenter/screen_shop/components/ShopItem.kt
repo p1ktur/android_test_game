fceadd0d647610ef248d06e852a79f9251cbe442
Renat Kovalov <renatkovalovofficial@gmail.com>
2023-07-14 16:05:54 +0300

    Final touches 2

--- app/src/main/java/com/company/testgame/feature_game/presenter/screen_shop/components/ShopItem.kt
+++ app/src/main/java/com/company/testgame/feature_game/presenter/screen_shop/components/ShopItem.kt
@@ -3,6 +3,7 @@ package com.company.testgame.feature_game.presenter.screen_shop.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -39,7 +41,10 @@ fun ShopItem(
     Box(
         modifier = modifier
             .padding(horizontal = 8.dp)
-            .clickable {
+            .clickable(
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            ) {
                 shopViewModel.selectSkin(skin)
             }
     ) {
